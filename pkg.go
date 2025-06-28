87dcaf40dbff008d864e09d89ba7575ceb2657f2
Admin <2762713521@qq.com>
2025-06-28 23:20:12 +0800

    dev

--- pkg.go
+++ pkg.go
@@ -130,7 +130,7 @@ func (flag) Set(name string, value bool) string {
 
 // FindMemory todo 特征码支持字节切片类型
 func (pattern) FindMemory(start int, size int, pattern string) (address uint) {
-	return request[uint]("Pattern/FindMem", map[string]string{"start": fmt.Sprintf("0x%x", start), "size": fmt.Sprintf("0x%x", size), "pattern": pattern})
+	return request[uint]("Pattern/FindMem", map[string]string{"start": fmt.Sprintf("0x%x", start), "size": fmt.Sprintf("%d", size), "pattern": pattern})
 }
 
 func (misc) ParseExpression(expression string) (value uint) {
@@ -146,8 +146,125 @@ type memoryBase struct {
 	Size        uint `json:"size"`
 }
 
-func (misc) FindMemoryBaseByAddress(address int) memoryBase {
+func (memory) FindBaseByAddress(address int) memoryBase {
 	return request[memoryBase]("MemoryBase", map[string]string{"addr": fmt.Sprintf("0x%x", address)})
 }
 
-func (misc) GetModuleList() {}
+type moduleInfo struct {
+	BaseAddress  uint `json:"base_address"`
+	Size         uint `json:"size"`
+	Entry        uint `json:"entry"`
+	SectionCount int  `json:"section_count"`
+	Name         string
+	Path         string
+}
+
+type moduleSectionInfo struct {
+	Address uint `json:"address"`
+	Size    uint `json:"size"`
+	Name    string
+}
+
+type moduleExport struct {
+	Ordinal         uint `json:"ordinal"`
+	Rva             uint `json:"rva"`
+	Va              uint `json:"va"`
+	Forwarded       bool `json:"forwarded"`
+	ForwardName     string
+	Name            string
+	UndecoratedName string
+}
+
+type moduleImport struct {
+	IatRva          uint `json:"iat_rva"`
+	IatVa           uint `json:"iat_va"`
+	Ordinal         uint `json:"ordinal"`
+	Name            string
+	UndecoratedName string
+}
+type module struct{}
+
+func (module) InfoFromAddr(address int) moduleInfo {
+	return request[moduleInfo]("Module/InfoFromAddr", map[string]string{"addr": fmt.Sprintf("0x%x", address)})
+}
+func (module) InfoFromName(name string) moduleInfo {
+	return request[moduleInfo]("Module/InfoFromName", map[string]string{"name": name})
+}
+func (module) BaseFromAddr(address int) uint {
+	return request[uint]("Module/BaseFromAddr", map[string]string{"addr": fmt.Sprintf("0x%x", address)})
+}
+func (module) BaseFromName(name string) uint {
+	return request[uint]("Module/BaseFromName", map[string]string{"name": name})
+}
+func (module) SizeFromAddr(address int) uint {
+	return request[uint]("Module/SizeFromAddr", map[string]string{"addr": fmt.Sprintf("0x%x", address)})
+}
+func (module) SizeFromName(name string) uint {
+	return request[uint]("Module/SizeFromName", map[string]string{"name": name})
+}
+func (module) NameFromAddr(address int) string {
+	return request[string]("Module/NameFromAddr", map[string]string{"addr": fmt.Sprintf("0x%x", address)})
+}
+func (module) PathFromAddr(address int) string {
+	return request[string]("Module/PathFromAddr", map[string]string{"addr": fmt.Sprintf("0x%x", address)})
+}
+func (module) PathFromName(name string) string {
+	return request[string]("Module/PathFromName", map[string]string{"name": name})
+}
+func (module) EntryFromAddr(address int) uint {
+	return request[uint]("Module/EntryFromAddr", map[string]string{"addr": fmt.Sprintf("0x%x", address)})
+}
+func (module) EntryFromName(name string) uint {
+	return request[uint]("Module/EntryFromName", map[string]string{"name": name})
+}
+func (module) SectionCountFromAddr(address int) int {
+	return request[int]("Module/SectionCountFromAddr", map[string]string{"addr": fmt.Sprintf("0x%x", address)})
+}
+func (module) SectionCountFromName(name string) int {
+	return request[int]("Module/SectionCountFromName", map[string]string{"name": name})
+}
+func (module) SectionFromAddr(address int, number int) moduleSectionInfo {
+	return request[moduleSectionInfo]("Module/SectionFromAddr", map[string]string{"addr": fmt.Sprintf("0x%x", address), "number": fmt.Sprintf("%d", number)})
+}
+func (module) SectionFromName(name string, number int) moduleSectionInfo {
+	return request[moduleSectionInfo]("Module/SectionFromName", map[string]string{"name": name, "number": fmt.Sprintf("%d", number)})
+}
+func (module) SectionListFromAddr(address int) []moduleSectionInfo {
+	return request[[]moduleSectionInfo]("Module/SectionListFromAddr", map[string]string{"addr": fmt.Sprintf("0x%x", address)})
+}
+func (module) SectionListFromName(name string) []moduleSectionInfo {
+	return request[[]moduleSectionInfo]("Module/SectionListFromName", map[string]string{"name": name})
+}
+func (module) GetMainModuleInfo() moduleInfo {
+	return request[moduleInfo]("Module/GetMainModuleInfo", nil)
+}
+func (module) GetMainModuleBase() uint {
+	return request[uint]("Module/GetMainModuleBase", nil)
+}
+func (module) GetMainModuleSize() uint {
+	return request[uint]("Module/GetMainModuleSize", nil)
+}
+func (module) GetMainModuleEntry() uint {
+	return request[uint]("Module/GetMainModuleEntry", nil)
+}
+func (module) GetMainModuleSectionCount() int {
+	return request[int]("Module/GetMainModuleSectionCount", nil)
+}
+func (module) GetMainModuleName() string {
+	return request[string]("Module/GetMainModuleName", nil)
+}
+func (module) GetMainModulePath() string {
+	return request[string]("Module/GetMainModulePath", nil)
+}
+func (module) GetMainModuleSectionList() []moduleSectionInfo {
+	return request[[]moduleSectionInfo]("Module/GetMainModuleSectionList", nil)
+}
+func (module) GetList() []moduleInfo {
+	return request[[]moduleInfo]("Module/GetList", nil)
+}
+func (module) GetExports(mod moduleInfo) []moduleExport {
+	return request[[]moduleExport]("Module/GetExports", map[string]string{"mod": fmt.Sprintf("%v", mod)})
+}
+func (module) GetImports(mod moduleInfo) []moduleImport {
+	return request[[]moduleImport]("Module/GetImports", map[string]string{"mod": fmt.Sprintf("%v", mod)})
+}
