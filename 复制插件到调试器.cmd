6385d9a386ed1a7b2610aca5662b564ea9b557d6
Admin <2762713521@qq.com>
2025-07-04 23:58:41 +0800

    dev

--- 复制插件到调试器.cmd
+++ 复制插件到调试器.cmd
@@ -1,3 +1,3 @@
-copy MCPx64dbg.dp32  release\x32
-copy MCPx64dbg.dp64  release\x64
+copy MCPx64dbg.dp32  release\x32\plugins\MCPx64dbg.dp32
+copy MCPx64dbg.dp64  release\x64\plugins\MCPx64dbg.dp64
 pause
